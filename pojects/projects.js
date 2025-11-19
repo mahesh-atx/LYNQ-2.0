87efa1b884fab2e95e652f9b06c6593b2938e3d7
Mahesh Dongare <maheshdongare49@gmail.com>
2025-11-19 18:40:22 +0530

    added portfolio link

--- pojects/projects.js
+++
@@ -1,7 +0,0 @@
-/*
-  projects.js
-  This file contains logic ONLY for the Projects page (projects.html).
-*/
-console.log("Projects script loaded.");
-
-// Add any projects-specific logic here
--- profile/profile.js
+++
@@ -1,8 +0,0 @@
-/*
-  profile.js
-  This file contains logic ONLY for the Profile page (profile.html).
-*/
-console.log("Profile script loaded.");
-
-// Add any profile-specific logic here
-// e.g., saving profile changes
